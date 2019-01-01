5f6cf1de1fe221699c0d7ec99dd11e5a608ffa03
shubhamshirsat <46106930+shubhamshirsat@users.noreply.github.com>
2019-01-01 16:34:42 +0530

    symbol table constructed

--- PassOne.java
+++ PassOne.java
@@ -9,21 +9,70 @@ public class PassOne {
 	
 	public void passOne() throws Exception
 	{
-		HashMap<String, Mnemonic> mot = getMnemonicOpcodeTable(new FileReader("optab.txt"));	// get the mot table
-		BufferedReader src = new BufferedReader(new FileReader("src.txt"));						// read the src file
+		HashMap<String, Mnemonic> mot = getMnemonicOpcodeTable(new FileReader("/home/student/workspace/TwoPassAssembler/src/optab.txt"));	// get the mot table
+		HashMap<String, Integer> symbolTable = new HashMap<String, Integer>();
+		
+		BufferedReader src = new BufferedReader(new FileReader("/home/student/workspace/TwoPassAssembler/src/src.txt"));						// read the src file
 
 
-		String line;
-		StringTokenizer token;
+		String line,token;
+		StringTokenizer stringTokenizer;
+		Mnemonic mnemonic = null;
+		boolean syntaxError = false;
+		int locationCounter = 0;
 
 		while((line = src.readLine()) != null)
 		{
-			token = new StringTokenizer(line.toUpperCase());											// break line into tokens
-			while(token.hasMoreTokens())
+			stringTokenizer = new StringTokenizer(line.toUpperCase());											// break line into tokens
+			
+			while(stringTokenizer.hasMoreTokens())
 			{
-				System.out.println("\n\n"+mot.containsKey(token.nextToken()));							// validate token
+				token = stringTokenizer.nextToken();
+				
+				if((!token.equalsIgnoreCase("START")) && locationCounter == 0)		// if first token is not start then it is syntax error
+				{
+					syntaxError = true;
+					break;			
+				}
+					
+								
+				if(mot.containsKey(token))							// validate token
+				{
+						mnemonic = mot.get(token);
+						
+						switch(mnemonic.getStatement())
+						{
+							case "AD":								// if token is assembler directive
+										if(token.equalsIgnoreCase("START"))
+										{
+											locationCounter = Integer.parseInt(stringTokenizer.nextToken())-1;
+										}
+									break;
+									
+							case "IS":								// if token is imperative statement
+									System.out.println("Imperative statement: "+token+"\noprand1: "+stringTokenizer.nextToken()+"\noperand2: "+stringTokenizer.nextToken());
+									break;
+									
+							case "DL":
+									break;
+						
+						}
+				}
+				else
+				{
+					System.out.println("Token is symbol: "+token);
+					symbolTable.put(token, locationCounter);					// if token is label then add to symbol table
+				}
+			}
+			locationCounter++;
+			if(syntaxError)
+			{
+				System.out.println("\n\nSTART not found");	
+				break;
 			}
 		}
+		
+		System.out.println("\n\nlocation counter: "+locationCounter);
 
 	}
 
